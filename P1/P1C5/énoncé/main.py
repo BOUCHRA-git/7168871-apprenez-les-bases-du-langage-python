5177e4df9f1e377eef685dfdf3c00df07581247c
BOUCHRA-git <amenhir.bouchra944@gmail.com>
2025-02-20 19:34:33 +0100

    main2.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,9 @@
-# Écrivez votre code ici !
+nom = "bouchra"
+age = 22
+taille = 1,65
+est_etudiant = True
+print(f"je m'appelle {nom} j'ai {age} j'ai {taille} et je {est_etudiant}")
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
