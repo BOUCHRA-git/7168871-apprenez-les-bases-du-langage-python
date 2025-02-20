12661b36b279a9643d6b874bad5b829e9abc4180
BOUCHRA-git <amenhir.bouchra944@gmail.com>
2025-02-20 20:12:37 +0100

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+fruits = ["pomme", "banane", "orange"]
+fruits.append("kiwi")
+fruits.remove("orange")
+fruits[1] = "ananas"
+print("la liste contient", len(fruits), "elements")
+fruits.sort()
+print(fruits)
